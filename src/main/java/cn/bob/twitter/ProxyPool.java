394098ed5a3a9caf24add081bb36d1b6cb341737
sunweibo <sunweibo@xiaomi.com>
2019-06-21 16:57:47 +0800

    first commit

---
+++ src/main/java/cn/bob/twitter/ProxyPool.java
@@ -0,0 +1,24 @@
+package cn.bob.twitter;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class ProxyPool  implements PageProcessor {
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(1).run();
+    }
+}
