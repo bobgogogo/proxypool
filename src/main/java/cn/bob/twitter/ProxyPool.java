1fac1891f3274c32771b1e70cdbee588817a243b
sunweibo <sunweibo@xiaomi.com>
2019-06-21 18:58:33 +0800

    fix:未完待续

--- src/main/java/cn/bob/twitter/ProxyPool.java
+++ src/main/java/cn/bob/twitter/ProxyPool.java
@@ -5,12 +5,49 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import net.sourceforge.tess4j.*;
+
+import javax.imageio.ImageIO;
+
 public class ProxyPool  implements PageProcessor {
     private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
 
     @Override
     public void process(Page page) {
+//        page.addTargetRequests(page.getHtml().links().regex("(https://proxy\\.mimvp\\.com/freeopen\\.php\\?proxy=out_hp&sort=p_checkdtime&page=\\d+)").all());
+        page.putField("ip",page.getHtml().xpath("//td[@class='tbl-proxy-ip']/text()").toString());
+        page.putField("port",code("https://proxy.mimvp.com/"+page.getHtml().xpath("//td[@class='tbl-proxy-port']/img/@src").toString()));
+        page.putField("type",page.getHtml().xpath("//td[@class='tbl-proxy-type']/text()").toString());
+        System.out.print(page.getResultItems());
+    }
 
+    public String code(String file) {
+        File imageFile = new File(file);
+
+        try {
+            BufferedImage image = ImageIO.read(imageFile);
+            ImageIO.write(image, "jpg",new File("tessdata/aaa"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        ITesseract instance = new Tesseract();
+        instance.setDatapath("tessdata"); // path to tessdata directory
+
+        try {
+            File image = new File("tessdata/aaa");
+            ImageIO.scanForPlugins();
+            String result = instance.doOCR(image);
+            System.out.println(result);
+            return result;
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+        String str = "no";
+        return str;
     }
 
     @Override
@@ -19,6 +56,9 @@ public class ProxyPool  implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(1).run();
+        ProxyPool pool = new ProxyPool();
+        Spider.create(pool)
+                .addUrl("https://proxy.mimvp.com/freeopen.php?proxy=out_hp&sort=p_checkdtime&page=1")
+                .run();
     }
 }
